bd92aa25195e7bf940d8580b113cc98277a36d4f
theharshitv <738vermaharshit@gmail.com>
2026-06-23 17:10:20 +0530

    added frontend demo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from db import get_or_create_conversation, update_conversation, escalate_conversation
 from router import handle_message
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -9,6 +10,12 @@ class ChatRequest(BaseModel):
     customer_id: str
     message: str
     channel: str = "whatsapp"
+
+
+@app.get("/")
+async def home():
+    with open("index.html", "r") as f:
+        return HTMLResponse(content=f.read())
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
